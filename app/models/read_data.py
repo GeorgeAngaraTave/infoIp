caf9f46b118c4c35840aa62ed2ea3dd428cd1db5
Jorge Angarita <jorge.angarita@servinformacion.com>
2020-03-13 16:32:07 -0500

    Se guarda cada peticón enun archivo para leerlo

---
+++ app/models/read_data.py
@@ -0,0 +1,62 @@
+import re
+import json
+import requests
+import socket
+import urllib
+from bs4 import BeautifulSoup
+import whois
+import csv, operator
+
+from flask import Flask, jsonify, request
+from app.controllers.settings.parser_data import *
+from app.controllers.settings.validate_data import *
+
+
+def infoIp(web):
+    if len(web) < 6:
+        return response(400, 'An error occurred. Please try again!', {'error': 'Ingrese un dirección web valida'})
+    else:        
+        socket.setdefaulttimeout(60)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        ip = socket.gethostbyname(web)        
+        
+        info = (urllib.request.urlopen("https://"+web))
+        print(info)
+        redditHtml = info.read()
+        soup = BeautifulSoup(redditHtml)
+
+        title = soup.find("head").find("title")
+        logo = ""
+        for links in soup.find_all("link", attrs={"rel": "icon"}):
+            logo = links.get('href')
+        
+        domain = whois.whois(web)
+        
+        result = {
+            "servers": domain.name_servers,
+            "address": ip,
+            "country": domain.country,
+            "whois": domain.whois_server,
+            "whois": domain.whois_server,
+            "owner": domain.registrar,
+            "servers_changed": True,
+            "logo": str(logo),
+            "title": str(title)
+        }
+        
+        archivo = open("C:/Users/JANGARITA.SERVINF/Documents/GEORGE/PRUEBA/api_/app/models/search.csv", mode='a')
+        archivo.write(web)
+        archivo.write(",")
+        archivo.close()
+        
+
+        return response(200, 'ok', result) 
+
+def queryInfo():   
+    with open("search.csv", mode="r") as csvarchivo:
+        entrada = csv.reader(csvarchivo)
+        print(entrada)
+        for reg in entrada:
+            reg = reg
+            for info in reg:
+                return response(200, 'ok', reg)          
---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+from app import app
+from app.controllers.settings.validate_data import *
+from app.controllers.settings.parser_data import *
+from app.models.read_data import *
+
+@app.route('/test')
+def ping():
+    return parserNumbers('123abcdef456')
+
+@app.route('/')
+def home():
+    return validator()
+
+@app.route('/analyze/<string:web>', methods=['GET'])
+def getInfoIp(web):  
+    return infoIp(web)
+
+@app.route('/query', methods=['GET'])
+def info():  
+    return queryInfo()        
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
